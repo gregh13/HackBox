e980998fd9fe4b1a94e7f1492fd50dbbb436ab68
Gregory Herringer <greg.herringer@gmail.com>
2024-08-06 14:58:15 -0700

    adds implementations for the new structure of the application (more dynamic/versatile, not Is-It-Cake specific)

--- src/main/java/com/isitcake/game/repositories/GameSessionRepository.java
+++ src/main/java/com/isitcake/game/repositories/GameSessionRepository.java
@@ -1,16 +1,27 @@
 package com.isitcake.game.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.isitcake.game.entities.GameSession;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
     Optional<GameSession> findBySessionId(String sessionId);
 
-    @Query("SELECT gs FROM GameSession gs WHERE gs.isActive = true")
+    @Query("SELECT gs FROM GameSession gs WHERE gs.active = true")
     List<GameSession> findAllActiveSessions();
+
+    @Query("SELECT gs FROM GameSession gs WHERE gs.dateCreated < :cutoffDate")
+    List<GameSession> findAllCreatedBefore(Timestamp cutoffDate);
+
+    @Modifying
+    @Query("DELETE FROM GameSession gs WHERE gs.active = false")
+    void deleteAllInactive();
 }
 
