8091d4282e7d50027d29628da0858b722c44e54c
Gregory Herringer <greg.herringer@gmail.com>
2024-08-06 15:04:19 -0700

    removes WebSocketConfig localhost as an allowed origin

--- src/main/java/com/isitcake/game/config/WebSocketConfig.java
+++ src/main/java/com/isitcake/game/config/WebSocketConfig.java
@@ -19,8 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
-                .setAllowedOriginPatterns("http://localhost:4200", "https://is-it-cake-play-along-frontend.vercel.app/")
-                .setAllowedOrigins("http://localhost:4200", "https://is-it-cake-play-along-frontend.vercel.app/");
+                .setAllowedOriginPatterns("https://is-it-cake-play-along-frontend.vercel.app/")
+                .setAllowedOrigins("https://is-it-cake-play-along-frontend.vercel.app/");
 
     }
 }
