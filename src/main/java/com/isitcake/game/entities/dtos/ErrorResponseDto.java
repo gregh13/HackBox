08097b8b2d0950f2d15ebc0671607120dd37214b
Gregory Herringer <greg.herringer@gmail.com>
2024-08-07 13:18:54 -0700

    updates ErrorResponseDto to use timestamps

--- src/main/java/com/isitcake/game/entities/dtos/ErrorResponseDto.java
+++ src/main/java/com/isitcake/game/entities/dtos/ErrorResponseDto.java
@@ -4,18 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponseDto {
     private String message;
-    private int status;
-    private long timestamp;
+    private Integer status;
+    private Timestamp timestamp;
 
     public ErrorResponseDto(String message, int status) {
         this.message = message;
         this.status = status;
-        this.timestamp = System.currentTimeMillis();
+        this.timestamp = new Timestamp(System.currentTimeMillis());
     }
 }
 
