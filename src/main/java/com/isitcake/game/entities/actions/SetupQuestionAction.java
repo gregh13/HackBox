261c07bc46460a86582f72e80529aa4b8b712b46
Gregory Herringer <greg.herringer@gmail.com>
2024-08-07 15:00:17 -0700

    adds JSON creator constructor to other Action classes

--- src/main/java/com/isitcake/game/entities/actions/SetupQuestionAction.java
+++ src/main/java/com/isitcake/game/entities/actions/SetupQuestionAction.java
@@ -1,6 +1,9 @@
 package com.isitcake.game.entities.actions;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.isitcake.game.entities.payloads.SetupQuestionActionPayload;
+import com.isitcake.game.enums.EventType;
 import lombok.Value;
 import lombok.EqualsAndHashCode;
 
@@ -8,4 +11,11 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper=true)
 public class SetupQuestionAction extends PlayerAction {
     SetupQuestionActionPayload payload;
+    @JsonCreator
+    public SetupQuestionAction(@JsonProperty("sessionId") String sessionId,
+                              @JsonProperty("eventType") EventType eventType,
+                              @JsonProperty("payload") SetupQuestionActionPayload payload) {
+        super(sessionId, eventType);
+        this.payload = payload;
+    }
 }
--- src/main/java/com/isitcake/game/entities/actions/TransitionStateAction.java
+++ src/main/java/com/isitcake/game/entities/actions/TransitionStateAction.java
@@ -1,6 +1,9 @@
 package com.isitcake.game.entities.actions;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.isitcake.game.entities.payloads.TransitionStateActionPayload;
+import com.isitcake.game.enums.EventType;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
@@ -8,4 +11,11 @@ import lombok.Value;
 @EqualsAndHashCode(callSuper=true)
 public class TransitionStateAction extends PlayerAction {
     TransitionStateActionPayload payload;
+    @JsonCreator
+    public TransitionStateAction(@JsonProperty("sessionId") String sessionId,
+                               @JsonProperty("eventType") EventType eventType,
+                               @JsonProperty("payload") TransitionStateActionPayload payload) {
+        super(sessionId, eventType);
+        this.payload = payload;
+    }
 }
