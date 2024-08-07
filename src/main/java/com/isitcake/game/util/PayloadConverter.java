9c1603154e0d7d3c7acb64564bd1c9047b7d7abf
Gregory Herringer <greg.herringer@gmail.com>
2024-08-07 10:17:41 -0700

    remove submit answer static method on Payload Converter

--- src/main/java/com/isitcake/game/util/PayloadConverter.java
+++ src/main/java/com/isitcake/game/util/PayloadConverter.java
@@ -2,8 +2,6 @@ package com.isitcake.game.util;
 
 import com.isitcake.game.entities.payloads.SetupQuestionActionPayload;
 import com.isitcake.game.entities.payloads.SetupQuestionResponsePayload;
-import com.isitcake.game.entities.payloads.SubmitAnswerActionPayload;
-import com.isitcake.game.entities.payloads.SubmitAnswerResponsePayload;
 
 public class PayloadConverter {
     public static SetupQuestionResponsePayload toResponsePayload(SetupQuestionActionPayload setupQuestionActionPayload){
@@ -12,14 +10,6 @@ public class PayloadConverter {
                 setupQuestionActionPayload.questionText(),
                 setupQuestionActionPayload.choices(),
                 setupQuestionActionPayload.timer()
-        );
-    }
-
-    public static SubmitAnswerResponsePayload toResponsePayload(SubmitAnswerActionPayload submitAnswerActionPayload){
-        return new SubmitAnswerResponsePayload(
-                submitAnswerActionPayload.playerName(),
-                submitAnswerActionPayload.choice(),
-                submitAnswerActionPayload.timeTaken()
         );
     }
 }
