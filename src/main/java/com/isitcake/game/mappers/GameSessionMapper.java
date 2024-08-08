328b40d4a7087be83e27c486881258f4ff63d239
Gregory Herringer <greg.herringer@gmail.com>
2024-08-08 12:51:33 -0700

    adds custom mapping to serialize the gamestate enum to its value attribute

--- src/main/java/com/isitcake/game/mappers/GameSessionMapper.java
+++ src/main/java/com/isitcake/game/mappers/GameSessionMapper.java
@@ -2,9 +2,18 @@ package com.isitcake.game.mappers;
 
 import com.isitcake.game.entities.GameSession;
 import com.isitcake.game.entities.dtos.GameSessionDto;
+import com.isitcake.game.enums.StateType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface GameSessionMapper {
+    @Mapping(source = "gameState", target = "gameState", qualifiedByName = "gameStateToString")
     GameSessionDto entityToDto(GameSession gameSession);
+
+    @Named("gameStateToString")
+    static String gameStateToString(StateType gameState) {
+        return gameState != null ? gameState.toString() : "unknown state";
+    }
 }
