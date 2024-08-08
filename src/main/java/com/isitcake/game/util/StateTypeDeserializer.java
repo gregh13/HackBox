176221922c343577da5c40251cef7ad607e448e5
Gregory Herringer <greg.herringer@gmail.com>
2024-08-08 10:24:11 -0700

    updates StateType enum and adds deserializer

---
+++ src/main/java/com/isitcake/game/util/StateTypeDeserializer.java
@@ -0,0 +1,17 @@
+package com.isitcake.game.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.isitcake.game.enums.StateType;
+
+import java.io.IOException;
+
+public class StateTypeDeserializer extends JsonDeserializer<StateType> {
+    @Override
+    public StateType deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        String value = jsonParser.getText();
+        return StateType.fromValue(value);
+    }
+}
