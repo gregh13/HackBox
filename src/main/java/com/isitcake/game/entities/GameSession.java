1c66a021c7aa67b8fd229437b28d68648a8c1529
Gregory Herringer <greg.herringer@gmail.com>
2024-08-09 20:38:32 -0700

    removes mappedBy parameter in annotation

--- src/main/java/com/isitcake/game/entities/GameSession.java
+++ src/main/java/com/isitcake/game/entities/GameSession.java
@@ -24,7 +24,7 @@ public class GameSession {
     private Timestamp dateCreated;
 
 
-    @OneToMany(mappedBy = "gameSession", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     List<Player> players  = new ArrayList<>();
 
