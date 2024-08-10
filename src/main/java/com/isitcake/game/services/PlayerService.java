86c0b3922bb1ca7ab53d6d8b84f9376668b81e01
Gregory Herringer <greg.herringer@gmail.com>
2024-08-09 18:55:38 -0700

    adds three methods to player service interface (resetPlayerChoices, transferHost, removeInactivePlayers) and implements each in service impl

--- src/main/java/com/isitcake/game/services/PlayerService.java
+++ src/main/java/com/isitcake/game/services/PlayerService.java
@@ -9,8 +9,14 @@ import java.util.List;
 public interface PlayerService {
     Player createPlayer(String playerName, GameSession gameSession, Boolean isHost);
 
-    Player updatePlayer(String playerName, List<Player> gameSessionPlayers, String choice, Double timeTaken);
+    Player updatePlayer(String sessionId, String playerName, String choice, Double timeTaken);
 
     PlayerDto getPlayerDto(Player player);
     List<PlayerDto> getPlayerDtos(List<Player> players);
+
+    List<Player> resetPlayerChoices(List<Player> players);
+
+    List<PlayerDto> transferHost(String sessionId, String playerName);
+
+    List<Player> removeInactivePlayers(List<Player> players);
 }
