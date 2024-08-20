0ed4a2a29cfd5964a1de8df97f88d3d48b4566eb
Gregory Herringer <greg.herringer@gmail.com>
2024-08-20 08:29:57 -0700

    adds player not found and null entity exception handlers

--- src/main/java/com/isitcake/game/controller/advice/ControllerAdvice.java
+++ src/main/java/com/isitcake/game/controller/advice/ControllerAdvice.java
@@ -1,9 +1,13 @@
 package com.isitcake.game.controller.advice;
 
 import com.isitcake.game.dto.response.error.GameSessionNotFoundResponse;
+import com.isitcake.game.dto.response.error.NullEntityResponse;
 import com.isitcake.game.dto.response.error.PlayerNameTakenResponse;
+import com.isitcake.game.dto.response.error.PlayerNotFoundResponse;
 import com.isitcake.game.exception.GameSessionNotFoundException;
+import com.isitcake.game.exception.NullEntityException;
 import com.isitcake.game.exception.PlayerNameTakenException;
+import com.isitcake.game.exception.PlayerNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,11 +16,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllerAdvice {
 
     @ExceptionHandler
+    public ResponseEntity<NullEntityResponse> handleNullEntityException(NullEntityException exc) {
+        // Create response variables
+        Integer status = HttpStatus.NOT_FOUND.value();
+        String error = "Null Entity Error";
+        String message = exc.getMessage();
+
+        // Create response body
+        NullEntityResponse nullEntityResponse = new NullEntityResponse(status, error, message);
+
+        // Create and return response entity
+        return new ResponseEntity<>(nullEntityResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
     public ResponseEntity<PlayerNameTakenResponse> handlePlayerNameTakenException(PlayerNameTakenException exc) {
         // Create response variables
         Integer status = HttpStatus.BAD_REQUEST.value();
         String error = "Player Name Taken";
-        String message = "The player name already exists in the game session and cannot be used again. Please try a new name to join the session.";
+        String message = exc.getMessage();
+
 
         // Create response body
         PlayerNameTakenResponse playerNameTakenResponse = new PlayerNameTakenResponse(status, error, message);
@@ -30,7 +49,7 @@ public class ControllerAdvice {
         // Create response variables
         Integer status = HttpStatus.NOT_FOUND.value();
         String error = "Game Session Not Found";
-        String message = "No game session was found with the given session id. Please try again after ensuring there are no typos or capitalization issues with the session id.";
+        String message = exc.getMessage();
 
         // Create response body
         GameSessionNotFoundResponse gameSessionNotFoundResponse = new GameSessionNotFoundResponse(status, error, message);
@@ -38,4 +57,19 @@ public class ControllerAdvice {
         // Create and return response entity
         return new ResponseEntity<>(gameSessionNotFoundResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<PlayerNotFoundResponse> handlePlayerNotFoundException(PlayerNotFoundException exc) {
+        // Create response variables
+        Integer status = HttpStatus.NOT_FOUND.value();
+        String error = "Player Not Found";
+        String message = exc.getMessage();
+
+        // Create response body
+        PlayerNotFoundResponse playerNotFoundResponse = new PlayerNotFoundResponse(status, error, message);
+
+        // Create and return response entity
+        return new ResponseEntity<>(playerNotFoundResponse, HttpStatus.NOT_FOUND);
+    }
+
 }
